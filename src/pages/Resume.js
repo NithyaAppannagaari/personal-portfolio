30baa2dd1eebc2961e74f63d2d0738e659af391d
Nithya <nithya.app@berkeley.edu>
2025-01-05 19:43:33 -0800

    lets do this again

---
+++ src/pages/Resume.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./Resume.css";
+
+const Resume = () => {
+    return (
+        <div class = "resume_class">
+            <div class = "title">
+                resume
+            </div> 
+            
+            <div class = "container">
+                <div class = "description">
+                    everything on my website in just one pdf.
+                </div> 
+            </div>
+            <div class = "pdf">
+                <embed 
+                    src="/resume.pdf" 
+                    type="application/pdf" 
+                    width="100%" 
+                    height="600px"
+                />
+            </div>  
+        </div>
+    );
+};
+
+export default Resume;
